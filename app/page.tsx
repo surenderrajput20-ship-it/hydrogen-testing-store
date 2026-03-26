dfd50245abfb33ea01ec07adffbee43bd56c5901
Surender Singh <surenderrajput20@gmail.com>
2026-03-26 12:28:04 +0530

    roll back

--- app/page.tsx
+++ app/page.tsx
@@ -1,86 +1,82 @@
 import { fetchAPI } from "../lib/shopify";
 import AddToCart from "../components/AddToCart";
-import Header from "../components/HeaderNew";
-
 export default async function Home() {
 
+
   const query = `
-  {
-    shop {
-      brand {
-        logo {
-          image {
-            url
-          }
-        }
-      }
-    }
-
-    menu(handle: "main-menu") {
-      items {
+ {
+  products(first: 8) {
+    edges {
+      node {
+        id
         title
-        url
-      }
-    }
-
-    products(first: 8) {
-      edges {
-        node {
-          id
-          title
-          handle
-          images(first:1){
-            edges{
-              node{
-                url
-              }
+        handle
+        images(first:1){
+          edges{
+            node{
+              url
             }
           }
-          variants(first:1){
-            edges{
-              node{
-                id
-                price{
-                  amount
-                }
+        }
+        variants(first:1){
+          edges{
+            node{
+             id   
+              price{
+                amount
+                currencyCode
+              }
+              compareAtPrice{
+              amount
               }
             }
           }
         }
+          
       }
     }
   }
+}
   `;
 
   const data = await fetchAPI(query);
-
-  const menu = data.data.menu.items;
-  const logo = data.data.shop.brand.logo.image.url;
   const products = data.data.products.edges;
+ 
 
   return (
-    <div>
+    <div className="p-10">
+      <h1 className="text-2xl font-bold mb-6">My Shopify Products tetts</h1>
 
-      {/* ✅ HEADER YAHAN HI HOGA */}
-      <Header menu={menu} logo={logo} />
-
-      <h1>My Shopify Products efrgfhn</h1>
-
+      {/* GRID */}
       <div className="grid grid-cols-4 gap-6">
-        {products.map(({ node }: any) => {
-          const variantId = node.variants.edges[0]?.node.id;
+       {products.map(({ node }: any) => {
 
-          return (
-            <div key={node.id}>
-              <img src={node.images.edges[0]?.node.url} />
-              <h2>{node.title}</h2>
+  const price = node.variants.edges[0]?.node.price.amount;
+  const compare = node.variants.edges[0]?.node.compareAtPrice?.amount;
+const variantId = node.variants.edges[0]?.node.id;
+  return (
+    <div key={node.id} className="border p-4 rounded">
+      <img src={node.images.edges[0]?.node.url} />
+      <h2 className="mt-2 font-semibold">{node.title}</h2>
 
-              <AddToCart variantId={variantId} />
-            </div>
-          );
-        })}
+      {/* PRICE UI */}
+      <div className="flex gap-2 items-center">
+        
+        {compare && (
+          <span className="line-through text-gray-500">
+            ₹{compare}
+          </span>
+        )}
+
+        <span className="font-bold text-lg">
+          ₹{price}
+        </span>
+<AddToCart variantId={variantId} />
       </div>
-
+    </div>
+  );
+})}
+      </div>
     </div>
   );
 }
